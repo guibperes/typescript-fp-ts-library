509888ab15b554adcd25dc71c8c00277dff5b897
Guilherme Beidaki Peres <guibperes@protonmail.com>
2021-05-19 14:18:37 -0300

    refactored service functions

--- src/base/service.ts
+++ src/base/service.ts
@@ -1,5 +1,3 @@
-import { pipe } from 'fp-ts/function';
-import { Either, fromOption } from 'fp-ts/Either';
 import { TaskEither } from 'fp-ts/TaskEither';
 
 import { Id, Repository } from '@/database';
@@ -7,39 +5,27 @@ import { ServiceError } from './error';
 
 export interface Service<E> {
   create: (entity: E) => TaskEither<ServiceError, Id>;
-  updateById: (id: string, entity: E) => Promise<Either<ServiceError, E>>;
-  deleteById: (id: string) => Promise<Either<ServiceError, boolean>>;
-  findById: (id: string) => Promise<Either<ServiceError, E>>;
+  updateById: (id: string, entity: E) => TaskEither<ServiceError, E>;
+  deleteById: (id: string) => TaskEither<ServiceError, boolean>;
+  findById: (id: string) => TaskEither<ServiceError, E>;
 }
 
 const create = <E>(repository: Repository<E>) => (
   entity: E,
 ): TaskEither<ServiceError, Id> => repository.create(entity);
 
-const updateById = <E>(repository: Repository<E>) => async (
+const updateById = <E>(repository: Repository<E>) => (
   id: string,
   entity: E,
-): Promise<Either<ServiceError, E>> =>
-  pipe(
-    await repository.updateById(id, entity),
-    fromOption(() => ({ error: 'Cannot update entity with provided id' })),
-  );
+): TaskEither<ServiceError, E> => repository.updateById(id, entity);
 
-const deleteById = <E>(repository: Repository<E>) => async (
+const deleteById = <E>(repository: Repository<E>) => (
   id: string,
-): Promise<Either<ServiceError, boolean>> =>
-  pipe(
-    await repository.deleteById(id),
-    fromOption(() => ({ error: 'Cannot delete entity with provided id' })),
-  );
+): TaskEither<ServiceError, boolean> => repository.deleteById(id);
 
-const findById = <E>(repository: Repository<E>) => async (
+const findById = <E>(repository: Repository<E>) => (
   id: string,
-): Promise<Either<ServiceError, E>> =>
-  pipe(
-    await repository.findById(id),
-    fromOption(() => ({ error: 'Cannot find entity with provided id' })),
-  );
+): TaskEither<ServiceError, E> => repository.findById(id);
 
 export const getService = <E>(repository: Repository<E>): Service<E> => ({
   create: create(repository),
